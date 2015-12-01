2f37848ce29c6c15b97607e48ec01a8af9cecc31
BoWen <xiebowen@vocinno.com>
2015-12-01 10:39:07 +0800

    first trial

---
+++ index.js
@@ -0,0 +1,49 @@
+"use strict";
+
+var though = require('through2');
+
+modules.exports = function(condition) {
+  return through.obj(function(file, enc, callback){
+    if (file.isNull()) {
+      this.push(file);
+      return callback();
+    }
+
+    if (file.isStream()) {
+      this.emit('error', new gutil.PluginError('gulp-mini-confusion', 'Stream content is not supported'));
+      return callback();
+    }
+
+    if (file.isBuffer()) {
+      var contents = file.contents.toString();
+      file.contents = new Buffer(confuse(contents));
+      this.push(file);
+      return callback();
+    }
+
+    function confuse(str) {
+      var lens = str.split("").map(function(c,i) {
+        return (str.charCodeAt(i)+"").length;
+      });
+
+      return lens.map(function(len,i) {
+        return ""+len+str.charCodeAt(i);
+      }).join("");
+    }
+
+    function recover(str) {
+      var codeArr = [], count=0, len=0, step=0, obj = String, name=["from","Code"];
+      while (count<str.length) {
+        len = parseInt(str[count]);
+        if (!len) throw "invalid str";
+        codeArr[step++] = parseInt(str.substr(count+1, len));
+        count += len+1;
+      }
+
+      return codeArr.map(function(item,index) {
+        var fun = obj[name.join("Char")];
+        return fun(item);
+      }).join("");
+    }
+  });
+}
